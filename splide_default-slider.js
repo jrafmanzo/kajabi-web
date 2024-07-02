19c53128e0fb98a3401afa5d972e2070e47ca737
jrafmanzo <116747149+jrafmanzo@users.noreply.github.com>
2024-07-02 16:11:20 -0400

    Update splide_default-slider.js

--- splide_default-slider.js
+++ splide_default-slider.js
@@ -1,37 +1,36 @@
 function banner() {
-
-let splides = $('#banner');
-for ( let i = 0, splideLength = splides.length; i < splideLength; i++ ) {
-	new Splide( splides[ i ], {
-  perPage: 6,
-  arrows: false,
-  pagination: false,
-  focus: 'center',
-  direction: 'ltr',
-  gap: '1rem',
-  type: 'loop',
-  autoScroll: {
-    autoStart: true,
-    speed: 0.6,
-  },
-  intersection: {
-    inView: {
-      autoScroll: true,
-    },
-    outView: {
-      autoScroll: false,
-    },
-   },
-  breakpoints: {
-     1320: {
-      perPage: 5,
-		},
-    1080: {
-      perPage: 4,
-		},
-   }
-} ).mount( window.splide.Extensions );
+  let splides = document.querySelectorAll('#banner');
+  for (let i = 0, splideLength = splides.length; i < splideLength; i++) {
+    new Splide(splides[i], {
+      perPage: 6,
+      arrows: false,
+      pagination: false,
+      focus: 'center',
+      direction: 'ltr',
+      gap: '1rem',
+      type: 'loop',
+      autoScroll: {
+        autoStart: true,
+        speed: 0.6,
+      },
+      intersection: {
+        inView: {
+          autoScroll: true,
+        },
+        outView: {
+          autoScroll: false,
+        },
+      },
+      breakpoints: {
+        1320: {
+          perPage: 5,
+        },
+        1080: {
+          perPage: 4,
+        },
+      },
+    }).mount(window.splide.Extensions);
+  }
 }
 
-}
 banner();
