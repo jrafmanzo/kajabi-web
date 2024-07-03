b821f831cfc50cc1ea36291290aa255396db63a6
jrafmanzo <116747149+jrafmanzo@users.noreply.github.com>
2024-07-03 12:25:15 -0400

    Create swiper_default-slider.js

---
+++ swiper_default-slider.js
@@ -0,0 +1,36 @@
+var defaultSwiper = new Swiper ('#swiper-default', {
+ 	loop: true,
+   loopFillGroupWithBlank: true,
+  slidesPerView: 1.5,
+  slidesPerGroup: 4,
+  grabCursor:true,
+  a11y: false,
+  spaceBetween: 28,
+  allowTouchMove: true,
+  navigation: {
+  nextEl: '#right-button',
+  prevEl: '#left-button',
+		},
+     breakpoints: {
+      0: { /* when window >=0px - webflow mobile landscape/portriat */
+          slidesPerView: 1,
+         slidesPerGroup:1,
+        spaceBetween: 28,
+      },
+          480: { /* when window >=0px - webflow mobile landscape/portriat */
+          slidesPerView: 1.2,
+         slidesPerGroup:1,
+        spaceBetween: 28,
+      },
+      767: { /* when window >= 767px - webflow tablet */
+            slidesPerView: 1.2,
+         slidesPerGroup:1,
+        spaceBetween: 28,
+      },
+      992: { /* when window >= 988px - webflow desktop */
+         slidesPerView: 1.5,
+         slidesPerGroup:1,
+        spaceBetween: 28,
+      }
+    },
+      });
