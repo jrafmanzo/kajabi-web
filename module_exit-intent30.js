c3038a522e0614ae71cbb56dfb2318d92b1a51dd
jrafmanzo <116747149+jrafmanzo@users.noreply.github.com>
2024-07-08 14:16:17 -0400

    Create module_exit-intent30.js

---
+++ module_exit-intent30.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    // Function to set a cookie
+    function setCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
+
+    // Function to get a cookie
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+
+    // Check if the cookie is already set
+    if (!getCookie('pricing100')) {
+        setTimeout(function() {
+            $('#exit-intent-component').removeClass('hide');
+            // Set a cookie that expires after the session ends
+            setCookie('pricing100', 'true', 0);
+        }, 29000); // 29000 milliseconds = 29 seconds
+    }
+});
