fcffa04e883ad579d4387d0301a63daf87bdd7ee
jrafmanzo <116747149+jrafmanzo@users.noreply.github.com>
2024-07-15 16:41:40 -0400

    Update component_image-enlarger.js

--- component_image-enlarger.js
+++ component_image-enlarger.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         if ($('#enlarge-modal').length === 0) {
             $('body').append(`
                 <div id="enlarge-modal" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%; padding:10%; box-sizing:border-box; background:rgba(0,0,0,0.8); justify-content:center; align-items:center; display:flex;">
-                    <img id="enlarged-image" src="" style="max-width:80vw; max-height:80vh; width:auto; height:auto; margin:auto;">
+                    <img id="enlarged-image" class="rich-text-image" src="" style="max-width:80vw; max-height:80vh; width:auto; height:auto; margin:auto;">
                 </div>
             `);
 
